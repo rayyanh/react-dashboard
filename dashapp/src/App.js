e2bb1d0105d2dd2452c6f0141b1f40ece8b5ad2d
Rayyan Hussain <rayyanhussain220@gmail.com>
2019-07-18 23:32:21 -0700

    small edit

--- dashapp/src/App.js
+++ dashapp/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-light"> Hello</nav>
+      <br />
+      <br />
+      <h4>Dashboard App</h4>
+      <br />
+      <div className="container">
+        <div className="row" />
+      </div>
     </div>
   );
 }
